043a11dd38f2ab25a73c1607c395d3de3992bd2c
neftaly polanco <polanconeftaly@gmail.com>
2020-05-12 12:47:54 -0400

    removed watch flag from jest test script

---
+++ src/__tests__/ers-user-repo.test.ts
@@ -0,0 +1,185 @@
+import { UserRepository } from '../repos/ers-user-repo';
+import * as mockIndex from '..';
+import * as mockMapper from '../util/result-set-mapper';
+import { User } from '../models/user';
+
+/*
+    We need to mock the connectionPool exported from the main module
+    of our application. At this time, we only use one exposed method
+    of the pg Pool API: connect. So we will provide a mock function 
+    in its place so that we can mock it in our tests.
+*/
+jest.mock('..', () => {
+    return {
+        connectionPool: {
+            connect: jest.fn()
+        }
+    }
+});
+
+// The result-set-mapper module also needs to be mocked
+jest.mock('../util/user-result-set-mapper', () => {
+    return {
+        mapUserResultSet: jest.fn()
+    }
+});
+
+describe('userRepo', () => {
+
+    let sut = new UserRepository();
+    let mockConnect = mockIndex.connectionPool.connect;
+
+    beforeEach(() => {
+
+        /*
+            We can provide a successful retrieval as the default mock implementation
+            since it is very verbose. We can provide alternative implementations for
+            the query and release methods in specific tests if needed.
+        */
+        (mockConnect as jest.Mock).mockClear().mockImplementation(() => {
+            return {
+                query: jest.fn().mockImplementation(() => {
+                    return {
+                        rows: [
+                            {
+                                id: 1,
+                                username: 'lreddick',
+                                password: 'password',
+                                first_name: 'Lance',
+                                last_name: 'Reddick',
+                                phone: '(257) 273-4567',                               email: 'aanderson@revature.com',
+                                role_id: 1
+                            }
+                        ]
+                    }
+                }), 
+                release: jest.fn()
+            }
+        });
+        (mockMapper.mapUserResultSet as jest.Mock).mockClear();
+    });
+
+    test('should resolve to an array of Users when getAll retrieves records from data source', async () => {
+        
+        // Arrange
+        expect.hasAssertions();
+
+        let mockUser = new User(1, 'un', 'pw', 'fn', 'ln', 'phone', 'email', 'locked');
+        (mockMapper.mapUserResultSet as jest.Mock).mockReturnValue(mockUser);
+
+        // Act
+        let result = await sut.getAll();
+
+        // Assert
+        expect(result).toBeTruthy();
+        expect(result instanceof Array).toBe(true);
+        expect(result.length).toBe(1);
+        expect(mockConnect).toBeCalledTimes(1);
+
+    });
+
+    test('should resolve to an empty array when getAll retrieves no records from data source', async () => {
+        
+        // Arrange
+        expect.hasAssertions();
+        (mockConnect as jest.Mock).mockImplementation(() => {
+            return {
+                query: jest.fn().mockImplementation(() => { return { rows: [] } }), 
+                release: jest.fn()
+            }
+        });
+
+        // Act
+        let result = await sut.getAll();
+
+        // Assert
+        expect(result).toBeTruthy();
+        expect(result instanceof Array).toBe(true);
+        expect(result.length).toBe(0);
+        expect(mockConnect).toBeCalledTimes(1);
+
+    });
+
+    test('should resolve to a User object when getById retrieves a record from data source', async () => {
+
+        // Arrange
+        expect.hasAssertions();
+
+        let mockUser = new User(1, 'un', 'pw', 'fn', 'ln', 'phone', 'email', 'locked');
+        (mockMapper.mapUserResultSet as jest.Mock).mockReturnValue(mockUser);
+
+        // Act
+        let result = await sut.getById(1);
+
+        // Assert
+        expect(result).toBeTruthy();
+        expect(result instanceof User).toBe(true);
+
+    });
+
+    test('should resolve to a User object when getUserByCredentials retrieves a record from data source', async () => {
+
+        // Arrange
+        expect.hasAssertions();
+
+        let mockUser = new User(1, 'un', 'pw', 'fn', 'ln', 'phone', 'email', 'locked');
+        (mockMapper.mapUserResultSet as jest.Mock).mockReturnValue(mockUser);
+
+        // Act
+        let result = await sut.getUserByCredentials('un', 'pw');
+
+        // Assert
+        expect(result).toBeTruthy();
+        expect(result instanceof User).toBe(true);
+
+    });
+
+    test('should resolve to a User object when getUserByUniqueKey retrieves a record from data source', async () => {
+
+        // Arrange
+        expect.hasAssertions();
+
+        let mockUser = new User(1, 'un', 'pw', 'fn', 'ln', 'phone', 'email', 'locked');
+         (mockMapper.mapUserResultSet as jest.Mock).mockReturnValue(mockUser);
+
+        // Act
+        let result = await sut.getUserByUniqueKey('username', 'myUser');
+
+        // Assert
+        expect(result).toBeTruthy();
+        expect(result instanceof User).toBe(true);
+
+    });
+
+    test('should add a new user when save is provided with valid input', async () => {
+
+        // Arrange
+        expect.hasAssertions();
+    
+        let mockUser = new User(1, 'un', 'pw', 'fn', 'ln', 'phone', 'email', 'locked');
+        (mockMapper.mapUserResultSet as jest.Mock).mockReturnValue(mockUser);
+    
+        // Act
+        let result = await sut.save(mockUser);
+    
+        // Assert
+        expect(result).toBeTruthy();
+        expect(result instanceof User).toBe(true);
+    
+    });
+
+    test('should delete a user when deleteById is provided with a user id', async () => {
+
+        // Arrange
+        expect.hasAssertions();
+
+        // Act
+        let result = await sut.deleteById(1);
+    
+        // Assert
+        expect(result).toBeTruthy();
+        expect(result).toBe(true);
+    
+    });
+
+});
