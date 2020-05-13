caea3fb779ec2a39fedfe5d474ef55725d8eceec
neftaly polanco <polanconeftaly@gmail.com>
2020-05-13 02:23:36 -0400

    added logic to cors-filter

---
+++ src/middleware/cors-filter.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+export function corsFilter(req: Request, resp: Response, next) {
+    resp.header('Access-Control-Allow-Origin', '*');
+    resp.header('Access-Control-Allow-Header', 'Origin, Content-Type, Accept');
+    resp.header('Access-Control-Allow-Credentials', 'true');
+    resp.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+
+    //return request with status 200 if it is an OPTIONS request
+    if (req.method === 'OPTIONS') {
+        resp.sendStatus(200);
+    } else {
+        next(); // passes the req and resp to the next middleware (or router)
+    }
+}
