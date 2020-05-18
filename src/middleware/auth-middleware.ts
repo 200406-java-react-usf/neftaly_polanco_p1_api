517de02a8b7602bab1da52f8e955319665f9bd77
neftaly polanco <polanconeftaly@gmail.com>
2020-05-18 03:33:42 -0400

    added FMGuard to auth middleware

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -4,17 +4,17 @@ import { AuthenticationError, AuthorizationError } from "../errors/errors";
 export const adminGuard = (request: Request, response: Response, next) => {
     if(!request.session.principal) {
         response.status(401).json(new AuthenticationError('No session found! Please provide valid login information.'));
-    } else if(request.session.principal.role_name === 'Admin' ) {
+    } else if(request.session.principal.role_name === 'ADMIN' ) {
         next();
     } else {
         response.status(403).json(new AuthorizationError());
     }
 }
 
-export const UserGuard = (request: Request, response: Response, next) => {
+export const FMGuard = (request: Request, response: Response, next) => {
     if(!request.session.principal) {
         response.status(401).json(new AuthenticationError('No session found! Please provide valid login information.'));
-    } else if(request.session.principal.role_name === 'Admin' || request.session.principal.role_name === 'Staff') {
+    } else if(request.session.principal.role_name === 'ADMIN' || request.session.principal.role_name === 'FINANCIAL MANAGER') {
         next();
     } else {
         response.status(403).json(new AuthorizationError());
